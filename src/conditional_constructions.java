15ca0eea93f2b63bed4e72abec9acfbc7028145b
vm24402 <vm24402@gmail.com>
2022-11-20 17:54:03 +0500

    ini

--- src/conditional_constructions.java
+++ src/conditional_constructions.java
@@ -23,4 +23,3 @@ public class conditional_constructions {
         }
 
     }
-}
---
+++ src/while_1.java
@@ -0,0 +1,12 @@
+public class while_1 {
+    public static void main(String[] args){
+   // boolean t = 1>3;
+        int value =0;
+        while(value<5)
+        {
+            System.out.println(value);
+            value=value+1;
+        }
+
+    }
+}
